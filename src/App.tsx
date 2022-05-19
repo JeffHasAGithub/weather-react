7e077917a699386b716500a128971d602b3970fb
Jeff <jeffmooreweb@gmail.com>
2022-05-19 18:25:38 -0500

    Fixed background resizing issue when WindowDisplay is resized

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { VStack } from "@chakra-ui/react";
 import { WeatherSearch } from "./components/WeatherSearch";
 import { WeatherHeading } from "./components/WeatherHeading";
@@ -9,22 +9,36 @@ import "./css/App.css";
 function App() {
   const [weather, setWeather] = useState<Weather | null>(null);
 
+  const root = document.getElementById("root")!;
+  useUpdateEffect(() => {
+    root.style.height = '100%';
+  }, [weather]);
+
   return (
     <>
-    <main>
-      <VStack spacing={24} mx={4} py={24}>
-        <WeatherSearch setWeather={setWeather} />
-        { weather && (
-            <VStack spacing={12} width="100%">
-              <WeatherHeading weather={weather} />
-              <WeatherDisplay weather={weather} />
-            </VStack>
-          )
-        }
-      </VStack>
-    </main>
+    <VStack spacing={24} mx={4} py={24} minHeight="100vh">
+      <WeatherSearch setWeather={setWeather} />
+      { weather && (
+          <VStack spacing={12} width="100%">
+            <WeatherHeading weather={weather} />
+            <WeatherDisplay weather={weather} />
+          </VStack>
+        )
+      }
+    </VStack>
     </>
   );
+}
+
+const useUpdateEffect: typeof useEffect = (effect, deps) =>{
+  const count = useRef(0);
+
+  useEffect(() => {
+    if (count.current > 1)
+      effect();
+    else
+      count.current++;
+  }, deps);
 }
 
 export default App;
