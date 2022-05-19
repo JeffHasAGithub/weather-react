0e6c38f05d59d936b968aea67ef5780d73850a00
Jeff <jeffmooreweb@gmail.com>
2022-05-19 12:28:09 -0500

    Added fields to Current model

--- src/models.ts
+++ src/models.ts
@@ -16,12 +16,19 @@ export interface Location {
 
 export interface Current {
   condition: Condition;
+  last_updated: string;
   temp_f: number;
   temp_c: number;
   feelslike_f: number;
   feelslike_c: number;
+  humidity: number;
+  cloud: number;
   wind_dir: string;
-  wind_mph: string;
+  wind_mph: number;
+  wind_kph: number;
+  vis_miles: number;
+  vis_km: number;
+  uv: number;
   is_day: number;
 }
 
