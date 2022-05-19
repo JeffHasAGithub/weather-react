a992a605adc8509ad552c2c7180f4aca52418713
Jeff <jeffmooreweb@gmail.com>
2022-05-19 14:03:24 -0500

    Finished layout for WeatherCurrent component

--- src/components/WeatherCurrent.tsx
+++ src/components/WeatherCurrent.tsx
@@ -1,8 +1,9 @@
-import { Heading, Image, VStack } from "@chakra-ui/react";
+import { Heading, Image, VStack, HStack } from "@chakra-ui/react";
 import { Flex, Square, Text, Spacer } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTemperatureHalf } from "@fortawesome/free-solid-svg-icons";
+import { faTemperatureHalf, faWind, faSun, faCloud, faWater, faEye } from "@fortawesome/free-solid-svg-icons";
 import { Current, Location } from "../models";
+import { jdate } from "../utils/jdate"
 
 interface Props {
   current: Current;
@@ -17,13 +18,16 @@ export function WeatherCurrent({ current, location, tempScale }: Props) {
     <Flex flexWrap="wrap">
       <VStack
         h="100%"
-        w="50%"
+        w={["100%", null, "49%"]}
         bg="gray.200"
         borderRadius="lg"
         spacing={8}
         p={4}
       >
-        <Heading>{current.condition.text}</Heading>
+        <VStack>
+          <Heading>{current.condition.text}</Heading>
+          <Text fontSize="lg">{jdate.toLocale(current.last_updated)}</Text>
+        </VStack>
         <Image src={current.condition.icon} alt="weather icon" />
         { tempScale === "F"
           ? <VStack>
@@ -37,8 +41,55 @@ export function WeatherCurrent({ current, location, tempScale }: Props) {
         }
       </VStack>
       <Spacer />
-      <VStack>
-        <Heading>{current.wind_mph}</Heading>
+      <VStack 
+        w={["100%", null, "49%"]} mt={[4, null, 0]}>
+        <Flex
+          h="100%"
+          w="100%"
+          bg="gray.200"
+          borderRadius="lg"
+          align="center"
+          fontSize="lg"
+          p={4}
+        >
+          <Text><FontAwesomeIcon icon={faWind} />{` ${current.wind_dir}`}</Text>
+          <Spacer />
+          <Text>{`${current.wind_mph} MPH`}</Text>
+          <Spacer />
+          <Text>{`${current.wind_kph} KPH`}</Text>
+        </Flex>
+        <Spacer />
+        <Flex
+          h="100%"
+          w="100%"
+          bg="gray.200"
+          borderRadius="lg"
+          align="center"
+          fontSize="lg"
+          p={4}
+        >
+          <FontAwesomeIcon icon={faEye} />
+          <Spacer />
+          <Text>{`${current.vis_miles} Miles`}</Text>
+          <Spacer />
+          <Text>{`${current.vis_km} Kilos`}</Text>
+        </Flex>
+        <Spacer />
+        <Flex
+          h="100%"
+          w="100%"
+          bg="gray.200"
+          borderRadius="lg"
+          align="center"
+          fontSize="lg"
+          p={4}
+        >
+          <Spacer />
+          <Text><FontAwesomeIcon icon={faCloud} />{` ${current.cloud}%`}</Text>
+          <Spacer />
+          <Text><FontAwesomeIcon icon={faWater} />{` ${current.humidity}%`}</Text>
+          <Spacer />
+        </Flex>
       </VStack>
     </Flex>
   );
