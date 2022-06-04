40d071d3ea3291c632e29d0f4b322c2b71fe7c98
Jeff <jeffmooreweb@gmail.com>
2022-06-04 15:10:41 -0500

    Moved weather components from App to new Weather component

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,11 @@
 import "./App.css"
 
-import React from "react"
-
-import WSearch from "./components/weather/wsearch"
-import WDisplay from "./components/weather/wdisplay"
-import WCurrent from "./components/weather/wcurrent"
-import WForecast from "./components/weather/wforecast"
-
-import * as Models from "./models"
+import Weather from "./components/weather"
 
 export default function App() {
-  const [weather, setWeather] = React.useState<Models.Weather | null>(null);
-
-  return (
-    <>
-			<WSearch setWeather={setWeather} />
-			{
-				weather &&
-					<>
-						<WDisplay
-							current={weather.current}
-							location={weather.location}
-						/> 
-						<WCurrent
-							current={weather.current}
-							location={weather.location}
-						/>
-						<WForecast
-							forecast={weather.forecast}
-							location={weather.location}
-						/>
-					</>
-			}
-    </>
-  );
+	return (
+		<>
+			<Weather />
+		</>
+	)
 }
---
+++ src/components/weather/index.tsx
@@ -0,0 +1,37 @@
+import styles from "./weather.module.css"
+
+import React from "react"
+
+import WSearch from "./wsearch"
+import WDisplay from "./wdisplay"
+import WCurrent from "./wcurrent"
+import WForecast from "./wforecast"
+
+import * as Models from "./models"
+
+export default function Weather() {
+  const [weather, setWeather] = React.useState<Models.Weather | null>(null);
+
+	return (
+		<>
+			<WSearch setWeather={setWeather} />
+			{
+				weather &&
+					<>
+						<WDisplay
+							current={weather.current}
+							location={weather.location}
+						/> 
+						<WCurrent
+							current={weather.current}
+							location={weather.location}
+						/>
+						<WForecast
+							forecast={weather.forecast}
+							location={weather.location}
+						/>
+					</>
+			}
+		</>
+	);
+}
